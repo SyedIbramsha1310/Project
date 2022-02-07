20b3dbe43736fadde94f989eec9b6e1c9dc7a355
Syed Ibramsha M <96477334+SyedIbramsha1310@users.noreply.github.com>
2022-02-07 11:54:06 +0530

    Delete Spring_Boot-FinalProject directory

--- Spring_Boot-FinalProject/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtfocus.assessment.service;
-
-import com.thoughtfocus.assessment.entity.ApplicationEntity;
-
-public interface LoginService {
-
-	ApplicationEntity validatelogindetails(String username, String password);
-
-}
